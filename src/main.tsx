a0898c8b47f15f1d56ce38a5d8619b3a6fbbc4ee
Muhammad Salman Ansar <salmanwattoo79@gmail.com>
2025-04-27 01:36:28 +0500

    Migrating this project to typescript

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClientInfoProvider } from "./context/supabaseClientInfo";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <AuthProvider>
     <ClientInfoProvider>
       <App />
